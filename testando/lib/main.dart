5021370fde25fe6f83f4250968873a4fcd82abbc
YtalloLima <ytalloaugusto@gmail.com>
2019-09-21 14:54:02 -0300

    Primeiro commit

---
+++ testando/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'models/itens.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Primeiro App"),
+        ),
+        body: Container(
+          child: Center(
+            child: Text("Olá mundo"),
+          ),
+        ));
+  }
+}
